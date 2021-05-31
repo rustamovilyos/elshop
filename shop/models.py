f2a9bd9280097965e39e24f207f2e5a720b92c08
rustamovilyos <rustamov7871@gmail.com>
2021-05-31 19:43:22 +0500

    new update

--- shop/models.py
+++ shop/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Toifa(models.Model):
-    nomi = models.CharField(max_length=50)
+    nomi = models.CharField('Toifa nomi', max_length=50)
 
 
 class Mahsulot(models.Model):
-    nomi = models.CharField(max_length=100)
+    nomi = models.CharField('Nomi', max_length=100)
     narx = models.FloatField()
     tarif = models.TextField(null=True)
     soni = models.IntegerField(default=10)
